d4f2edf6cf3a206326aa136cd09c1001e28019a7
Cerf <cerf_fx@yahoo.com>
2021-07-08 08:40:42 -0500

    commit B

--- src/main/java/com/probando/gith/App.java
+++ src/main/java/com/probando/gith/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola commit 2" );
+        System.out.println( "Hola " );
+        System.out.println( " commit B Julio 8" );
     }
 }
